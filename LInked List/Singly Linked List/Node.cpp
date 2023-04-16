666a86f4b3cbedd2a9135e33c6d5ca70e2a643bf
Abdalla Rabea MAbed <108424011+AbdallaRabeaMabed@users.noreply.github.com>
2023-04-16 05:49:34 +0200

    Update Node.cpp

--- LInked List/Singly Linked List/Node.cpp
+++ LInked List/Singly Linked List/Node.cpp
@@ -1,57 +1,58 @@
+
 #include <iostream>
-#include <cassert>
+
 using namespace std;
-
-
-struct Node {
-	int data {};
-	Node* next {};
-	// Pointer to SAME type
-	Node(int data) : data(data) {}
+struct Node{
+    int data = 0;
+    Node* next = nullptr;
+    Node(int D)
+    {
+        data = D;
+    }
 };
-
-void print1(Node* head) {
-	while(head != nullptr) {
-		cout<<head->data<<" ";
-		head = head->next;
-	}
-	cout<<"\n";
+void print(Node* head)
+{
+    while(head!= nullptr)
+    {
+        cout<< head->data << endl;
+        head = head->next ;
+    }
 }
-
-void print2(Node* head) {		// print forward recursively
-	if (head == nullptr) {
-		cout<<"\n";
-		return;
-	}
-	cout<<head->data<<" ";
-	print2(head->next);
+void print2(Node* head)
+{
+	// print node recursively
+    if(head == nullptr)
+    {
+        cout<< "\n";
+        return;
+    }
+    cout<< head->data << endl;
+    print2(head->next);
 }
-
-void print3_reversed(Node* head) {		// print backward recursively
-	if (head == nullptr) {
-		return;
-	}
-	print3_reversed(head->next);
-	cout<<head->data<<" ";
+void print_reverse(Node* head)
+{
+    if(head == nullptr)
+    {
+        return;
+    }
+    print_reverse(head->next);
+    cout<< head->data << "\n";
 }
-
-int main() {
-
-	Node* node1 = new Node(6);
-	Node* node2 = new Node(10);
-	Node* node3 = new Node(8);
-	Node* node4 = new Node(15);
-
-	node1->next = node2;
-	node2->next = node3;
-	node3->next = node4;
-	node4->next = nullptr;
-
-	print1(node1);
-
-
-	print2(node1);
-	print3_reversed(node1);
-
-	return 0;
+int main()
+{
+    Node* node1 = new Node(5);
+    Node* node2 = new Node(4);
+    Node* node3= new Node(3);
+    Node* node4 = new Node(2);
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = nullptr;
+    print(node1);
+    cout<< "=======================\n";
+    print2(node1);
+    cout<< "=======================\n";
+    print_reverse(node1);
+    
+    return 0;
 }
